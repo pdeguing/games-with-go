8b8d7a970ebb3b1795b3df66ccf800163ecc5739
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-05-13 15:56:05 -0700

    Printing a beautiful gradient

--- sdl2/sdl2.go
+++ sdl2/sdl2.go
@@ -6,6 +6,22 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const	winWidth, winHeight int = 1000, 600
+
+type color struct {
+	r, g, b byte
+}
+
+func setPixel(x, y int, c color, pixels []byte) {
+	index := (y * winWidth + x) * 4
+
+	if index < len(pixels) - 4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index + 1] = c.g
+		pixels[index + 2] = c.b
+	}
+}
+
 func main() {
 
 	err := sdl.Init(sdl.INIT_EVERYTHING)
@@ -16,12 +32,41 @@ func main() {
 	defer sdl.Quit()
 
 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+		sdl.WINDOWPOS_UNDEFINED, int32(winWidth), int32(winHeight),
+		sdl.WINDOW_SHOWN)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888,
+		sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tex.Destroy()
+
+	pixels := make([]byte, winWidth * winHeight * 4)
+
+	for y := 0; y < winHeight; y++ {
+		for x := 0; x < winWidth; x++ {
+			setPixel(x, y, color{byte((x / (winWidth / 255)) % 255),
+			byte((y / (winWidth / 255)) % 255),
+			byte((x * y) % 255)}, pixels)
+		}
+	}
+	tex.Update(nil, pixels, winWidth * 4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
 
 	for {
 		for event := sdl.PollEvent(); event != nil; sdl.PollEvent() {
